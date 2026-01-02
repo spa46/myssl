ad8366f6be53b6df1cacddb3e48902e3cd0d485e
seansc.park <psckyky@hotmail.com>
2026-01-02 11:05:15 +0900

    [enhancement] crl/oscp

--- sniclient_seq_test.py
+++
@@ -1,7 +0,0 @@
-openssl s_client -connect localhost:4443 -servername valid.example.com -CAfile serverCA/ca-chain.pem
-
-openssl s_client -connect localhost:4443 -servername expired.example.com -CAfile serverCA/ca-chain.pem
-
-openssl s_client -connect localhost:4443 -servername revoked.example.com -CAfile serverCA/ca-chain.pem
-
-openssl s_client -connect localhost:4443 -servername selfsigned.example.com -CAfile serverCA/ca-chain.pem
